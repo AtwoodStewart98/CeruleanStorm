fe7847d1b976693f17ada51032cbd473620ace15
Stewart Atwood <stewartatwood@Stewarts-MacBook-Pro.local>
2017-10-10 10:27:21 -0500

    removed extraneous js file

--- public/router.js
+++ public/router.js
@@ -1,21 +1,32 @@
-angular.module("ceruleanstorm").config(($urlRouterProvider, $stateProvider) => {
-  $urlRouterProvider.otherwise("/");
-  $stateProvider
-    .state("home", {
-      url: "/",
-      templateUrl: "./components/home/homeTmpl.html"
-      // controller: "homeCtrl"
-      // resolve: {
-      //   user: mainSrvc =>
-      //     mainSrvc
-      //       .getUser()
-      //       .then(response => response.data)
-      //       .catch(err => err)
-      // }
-    })
-    .state("music", {
-      url: "/music",
-      templateUrl: "./components/artists-music/musicTmpl.html",
-      controller: "musicCtrl"
-    });
-});
+angular
+  .module("ceruleanstorm", ["ui.router"])
+  .config(($urlRouterProvider, $stateProvider) => {
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+      .state("home", {
+        url: "/",
+        templateUrl: "./components/home/homeTmpl.html"
+        // controller: "homeCtrl"
+        // resolve: {
+        //   user: mainSrvc =>
+        //     mainSrvc
+        //       .getUser()
+        //       .then(response => response.data)
+        //       .catch(err => err)
+        // }
+      })
+      .state("music", {
+        url: "/music",
+        templateUrl: "./components/artists-music/musicTmpl.html",
+        controller: "musicCtrl"
+      })
+      .state("art", {
+        url: "/visual-art",
+        templateUrl: "./components/visual-art/artTmpl.html",
+        controller: "artCtrl"
+      })
+      .state("writing", {
+        url: "/writing-forum",
+        templateUrl: "./components/writing/writingTmpl.html"
+      });
+  });
